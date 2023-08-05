5f628dff813def6a406fa34337fe3572d086ac83
adrian <adrianmouzinhopro@gmail.com>
2023-08-05 10:36:14 -0300

    aula 17

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Me livrando da maldição</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "Olá, mundo! \u{1F30E}😎";
+    ?>
+  </h1>
+
+  <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
